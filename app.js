0d6e677e421d7e883c57bd3975303a027127982d
amitpoitrix <amit.poitrix@gmail.com>
2024-07-06 19:45:43 +0530

    Added DB connection along with env variables

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use("/health", (req, res, next) => {
+    console.log("Incoming v1/health");
     res.status(200).json({message: "social media service is up and running"});
-})
-
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
 });
+
+mongoose
+    .connect(process.env.DB_CONNECT)
+    .then(() => {
+        console.log("MongoDB is connected");
+    }).catch((err) => {
+        console.log(err);
+    });
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on port ${process.env.PORT}`);
+})
